fecd2e370a5de60f9736f990175a5c5a61dfc8ee
Luisa María Puentes Torres <luchis.puentes@gmail.com>
2026-03-01 21:42:18 -0500

    first commit

---
+++ server/db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if(err){
+        console.log('Error al conectar la base de datos', err)
+    }
+    
+    console.log('Conexión exitosa a MySQL');
+
+});
+
+module.exports = connection;
+
